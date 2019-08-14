61a7bc9f2b6e9987361c62da6caadddca9d7ba5a
Israel Schallheim <schall@campus.technion.ac.il>
2019-08-14 20:30:46 +0300

    cleaned unused files

--- test_run.py
+++
@@ -1,8 +0,0 @@
-import gym
-env = gym.make('BipedalWalkerHardcore-v2') # try also CartPole-v0,MountainCar-v0,MsPacman-v0,Hopper-v1
-env.reset()
-for _ in range(1000):
-    env.render()
-    ob, r, done, info = env.step(env.action_space.sample()) # take a random action
-    if done:
-        ob = env.reset()
